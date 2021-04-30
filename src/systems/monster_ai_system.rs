e8f1a758dc8e174329533b3bed49ac7b122c0245
Bernard Cloutier <Bernard.P.Cloutier@outlook.com>
2021-04-30 17:22:56 -0400

    Tweaks monster ai system.

--- src/systems/monster_ai_system.rs
+++ src/systems/monster_ai_system.rs
@@ -1,22 +1,21 @@
-use crate::{Monster, Name, Position, Viewshed};
-use rltk::{console, Point};
+use crate::{Monster, Name, PlayerPosition, Viewshed};
+use rltk::console;
 use specs::prelude::*;
 
 pub struct MonsterAISystem {}
 
 impl<'a> System<'a> for MonsterAISystem {
     type SystemData = (
-        ReadExpect<'a, Point>,
+        ReadExpect<'a, PlayerPosition>,
         ReadStorage<'a, Viewshed>,
-        ReadStorage<'a, Position>,
         ReadStorage<'a, Monster>,
         ReadStorage<'a, Name>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (player_pos, viewshed, pos, monster, name) = data;
+        let (player_pos, viewshed, monster, name) = data;
 
-        for (viewshed, pos, _monster, name) in (&viewshed, &pos, &monster, &name).join() {
+        for (viewshed, _monster, name) in (&viewshed, &monster, &name).join() {
             if viewshed.visible_tiles.contains(&*player_pos) {
                 console::log(format!("{} considers their own existence", name.name));
             }
